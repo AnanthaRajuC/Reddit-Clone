b9b989542e0f7bd606a27635954769a43e8b705e
Anantha Raju C <arcswdev@gmail.com>
2020-11-07 11:36:54 +0530

    Initial Commit

---
+++ src/main/java/io/github/anantharajuc/rc/RedditCloneApplication.java
@@ -0,0 +1,13 @@
+package io.github.anantharajuc.rc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedditCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedditCloneApplication.class, args);
+	}
+
+}
